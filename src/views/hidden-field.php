596c1ab2e2fc7b071c8fd7ffa457448df533409e
Clifford Paulick <tko@tourkick.com>
2020-07-27 18:11:27 -0500

    formatting

--- src/views/hidden-field.php
+++ src/views/hidden-field.php
@@ -1,8 +1,11 @@
 <td>
 	<div>
-		<?php $hidden = (array) get_post_meta( get_the_ID(), 'custom-hidden', true );
+		<?php
+		$hidden     = (array) get_post_meta( get_the_ID(), 'custom-hidden', true );
 		$field_name = esc_attr( $customField['name'] );
 		?>
-		<label><?php esc_html_e( 'Hide field', 'tribe-ext-hide-additional-fields' ); ?><input type="checkbox" value="<?php echo $field_name ?>" name="custom-hidden[]" <?php checked( in_array( $field_name, $hidden, true ) ); ?> />
-		</label></div>
+		<label><?php esc_html_e( 'Hide field', 'tribe-ext-hide-additional-fields' ); ?>
+			<input type="checkbox" value="<?php echo $field_name ?>" name="custom-hidden[]" <?php checked( in_array( $field_name, $hidden, true ) ); ?> />
+		</label>
+	</div>
 </td>
