24e01ee869efef6062a39669357d2302e893eb7d
András Guseo <andras@divi-magazine.com>
2021-01-19 12:23:50 +0100

    Added filtering for the starting states for the hide options
    All 'hide field' checkboxes can now be set to default hidden with `add_filter( 'tribe_ext_hide_additional_fields_default_hidden', '__return_true' );`

--- src/views/hidden-field.php
+++ src/views/hidden-field.php
@@ -3,9 +3,26 @@
 		<?php
 		$hidden     = (array) get_post_meta( get_the_ID(), Tribe__Extension__Hide_Additional_Fields::$field_key, true );
 		$field_name = esc_attr( $customField['name'] );
+
+		/**
+		 * Allow filtering whether the checkboxes should be checked by default.
+		 *
+		 * @var bool $start_hidden
+		 */
+		$start_hidden = apply_filters( 'tribe_ext_hide_additional_fields_default_hidden', false );
+
+		if (
+			$start_hidden
+			&& get_post_status() == 'auto-draft'
+		) {
+			$hide_field_status = 'checked="checked"';
+		}
+		else {
+			$hide_field_status = checked( in_array( $field_name, $hidden, true ), true, false );
+		}
 		?>
 		<label><?php esc_html_e( 'Hide field', 'tribe-ext-hide-additional-fields' ); ?>
-			<input type="checkbox" value="<?php echo $field_name ?>" name="<?php echo Tribe__Extension__Hide_Additional_Fields::$field_key; ?>[]" <?php checked( in_array( $field_name, $hidden, true ) ); ?> />
+			<input type="checkbox" value="<?php echo $field_name ?>" name="<?php echo Tribe__Extension__Hide_Additional_Fields::$field_key; ?>[]" <?php echo $hide_field_status ?> />
 		</label>
 	</div>
 </td>
