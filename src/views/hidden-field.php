496b3af389be3bc265641fbddd98667d0fdb8895
András Guseo <andras@divi-magazine.com>
2021-01-28 15:07:41 +0100

    For readability re-worked how $hide_field_status is checked

--- src/views/hidden-field.php
+++ src/views/hidden-field.php
@@ -11,20 +11,25 @@
 		 *
 		 * @var bool $start_hidden
 		 */
-		$start_hidden = (booL) apply_filters( 'tribe_ext_hide_additional_fields_default_hidden', false );
+		$start_hidden = (bool) apply_filters( 'tribe_ext_hide_additional_fields_default_hidden', false );
 
 		if (
 			$start_hidden
 			&& 'auto-draft' === get_post_status()
 		) {
-			$hide_field_status = 'checked="checked"';
+			$hide_field_status = true;
 		}
 		else {
-			$hide_field_status = checked( in_array( $field_name, $hidden, true ), true, false );
+			$hide_field_status = false;
 		}
 		?>
 		<label style="vertical-align: top; line-height: 1em;"><?php esc_html_e( 'Hide field', 'tribe-ext-hide-additional-fields' ); ?>
-			<input type="checkbox" value="<?php echo $field_name ?>" name="<?php echo Tribe__Extension__Hide_Additional_Fields::$field_key; ?>[]" <?php echo $hide_field_status ?> />
+			<input
+				type="checkbox"
+				value="<?php echo $field_name ?>"
+				name="<?php echo Tribe__Extension__Hide_Additional_Fields::$field_key; ?>[]"
+				checked="<?php echo $hide_field_status ? 'checked' : checked( in_array( $field_name, $hidden, true ), true, false ); ?>"
+			/>
 		</label>
 	</div>
 </td>
